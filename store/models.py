0989c6d5db5d28290a053039481aed19d801e372
Giya001 <giyoshikmatillayev@gmail.com>
2025-07-31 14:02:23 +0500

    models changed

--- store/models.py
+++ store/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,unique=True)
     parent = models.ForeignKey('self', on_delete=models.CASCADE,related_name='subcategories',blank=True,null=True)
 
     def __str__(self):
